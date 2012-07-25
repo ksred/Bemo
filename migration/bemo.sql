112aa17926c146a2135dda3a4b767585b126ec9a
Monkey D Luffy <luffy@luffy.(none)>
2012-07-25 13:41:55 +0200

    Added database structure and initial data

---
+++ migration/bemo.sql
@@ -0,0 +1,193 @@
+-- phpMyAdmin SQL Dump
+-- version 3.4.10.1deb1
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Jul 25, 2012 at 01:40 PM
+-- Server version: 5.5.24
+-- PHP Version: 5.3.14
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `bemo`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `artist`
+--
+
+CREATE TABLE IF NOT EXISTS `artist` (
+  `id` int(100) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  `desc` varchar(500) NOT NULL,
+  `social_lastfm` int(1) NOT NULL DEFAULT '0',
+  `social_youtube` int(1) NOT NULL DEFAULT '0',
+  `social_vimeo` int(1) NOT NULL DEFAULT '0',
+  `social_soundcloud` int(1) NOT NULL DEFAULT '0',
+  `social_bandcamp` int(1) NOT NULL DEFAULT '0',
+  `social_facebook` int(1) NOT NULL DEFAULT '0',
+  `social_twitter` int(1) NOT NULL DEFAULT '0',
+  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `artist_social`
+--
+
+CREATE TABLE IF NOT EXISTS `artist_social` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `artist_id` int(10) NOT NULL,
+  `social_id` int(1) NOT NULL,
+  `desc` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `campaign`
+--
+
+CREATE TABLE IF NOT EXISTS `campaign` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `title` varchar(50) NOT NULL,
+  `desc` varchar(1000) NOT NULL,
+  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `campaign_artists`
+--
+
+CREATE TABLE IF NOT EXISTS `campaign_artists` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `campaign_id` int(10) NOT NULL,
+  `artist_id` int(10) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `payment`
+--
+
+CREATE TABLE IF NOT EXISTS `payment` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `artist_id` int(10) NOT NULL,
+  `campaign_id` int(10) NOT NULL,
+  `payment_id` int(10) NOT NULL,
+  `amount` float NOT NULL,
+  `status` int(1) NOT NULL,
+  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `social`
+--
+
+CREATE TABLE IF NOT EXISTS `social` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `title` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=8 ;
+
+--
+-- Dumping data for table `social`
+--
+
+INSERT INTO `social` (`id`, `title`) VALUES
+(1, 'LastFm'),
+(2, 'Vimeo'),
+(3, 'Youtube'),
+(4, 'Facebook'),
+(5, 'Twitter'),
+(6, 'Soundcloud'),
+(7, 'Bandcamp');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` int(100) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `description` varchar(500) NOT NULL,
+  `role` int(10) NOT NULL,
+  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id` (`id`,`email`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `name`, `password`, `email`, `description`, `role`, `date`) VALUES
+(1, 'Bemo', '05ad5d715855a16b1b91a58dd9ad7b09', 'bemo@bemo.fm', 'I am Bemo', 1, '2012-07-24 19:20:16');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_role`
+--
+
+CREATE TABLE IF NOT EXISTS `user_role` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `title` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
+
+--
+-- Dumping data for table `user_role`
+--
+
+INSERT INTO `user_role` (`id`, `title`) VALUES
+(1, 'Admin'),
+(2, 'Label'),
+(3, 'Artist'),
+(4, 'Promoter'),
+(5, 'User');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `votes`
+--
+
+CREATE TABLE IF NOT EXISTS `votes` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `artist_id` int(10) NOT NULL,
+  `campaign_id` int(10) NOT NULL,
+  `round` int(1) NOT NULL,
+  `ip` varchar(50) NOT NULL,
+  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
