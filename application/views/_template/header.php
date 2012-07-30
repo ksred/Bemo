cc70153d7d335dc5999d4e4c7ac49d0695d3dcef
Kyle Red <awesomistguy@gmail.com>
2012-07-30 07:34:45 +0200

    Fixed relative stylesheet paths

--- application/views/_template/header.php
+++ application/views/_template/header.php
@@ -6,9 +6,9 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <link href="../../assets/css/bootstrap.css" rel="stylesheet">
-    <link href="../../assets/css/bemo.css" rel="stylesheet">
-    <link href="../../assets/css/bootstrap-responsive.css" rel="stylesheet">
+    <link href="<?= BASE_URL ?>/assets/css/bootstrap.css" rel="stylesheet">
+    <link href="<?= BASE_URL ?>/assets/css/bemo.css" rel="stylesheet">
+    <link href="<?= BASE_URL ?>/assets/css/bootstrap-responsive.css" rel="stylesheet">
 
     <!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
     <!--[if lt IE 9]>
