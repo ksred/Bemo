a2b86d99c2ace7ae34d0fcb367d249c0653a6a45
Kyle Red <awesomistguy@gmail.com>
2012-10-07 13:51:41 +0200

    Added new construction page of awesomeness

--- application/web/views/construction.php
+++ application/web/views/construction.php
@@ -1,31 +1,43 @@
+
 <!DOCTYPE html>
 <html>
     <head>
         <title>Bemo!fm</title>
 	<link rel="stylesheet" href="<?= BASE_URL ?>/assets/css/bootstrap.min.css" />
+    <script src="<?= BASE_URL ?>/assets/js/jquery.js"></script>
 	<!--<link href="http://cdn-images.mailchimp.com/embedcode/slim-081711.css" rel="stylesheet" type="text/css">-->
     </head>
     <body>
-        <style>
-            body {
-                background: black;
-                background-image: url("/assets/img/bg3.png");
-            }
-            
-            .container {
-                width: 960px;
-                margin: auto;
-            }
-            
-            .container img {
-                margin: 60px 0px 0px 0px;
-            }
-	    
-	    .well {
-		background-color: rgba(0, 0, 0, 0.5);
-	    }
-	    
-	    #mc_embed_signup{
+    <style>
+    body {
+    	background-image: url('/images/landing/bg.jpg');
+    	background-color: #64982c;
+    	background-position: center top;
+    	overflow: hidden;
+    }
+
+    #mascot {
+    	width: 800px;
+    	margin: 0 auto;
+    	height: auto;
+    	position: relative;
+    }
+    #mascotwrap {
+    	width: 800px;
+    	margin: 0 auto;
+    	height: auto;
+    }
+    #logo {
+    	height: 0px;
+    	width: 400px;
+    	margin: 0 auto;
+    }
+	.well {
+		background-color: rgba(0, 0, 0, 0.1);
+	}
+	
+	#mc_embed_signup{
+		position: relative;
 		clear:left;
 		color: white;
 		text-shadow: 0px 0px 4px #000;
@@ -34,23 +46,37 @@
 		text-align: center;
 		margin: 0 auto;
 		width: 400px;
-	    }
-	    
-	    #mc_embed_signup label {
+	}
+	
+	#mc_embed_signup label {
 		font-size: 20px;
 		line-height: 30px;
-	    }
-	    
-	    #mc_embed_signup .email{
+	}
+	
+	#mc_embed_signup .email{
 		border-radius: 10px;
 		padding: 5px;
 		margin-bottom: 20px;
-	    }
-        </style>
-        
-        <div class="container">
-           <center><img src="<?= BASE_URL ?>/assets/img/bemologo.png" /></center>
-	   
+	}
+    </style>
+    <script>
+    	$(document).ready( function() {
+    		var height = $(window).height();
+    		$('#mascot').css({top : height - 800});
+    		$('#mc_embed_signup').css({top : height - 500});
+    		
+    		$('#mascotwrap').hover( function() {
+				$('#mascot').animate({top : height - 400}, 500);
+			}, function() {
+				$('#mascot').animate({top : height - 800}, 500);
+			});
+    	});
+    </script>
+
+    <div id="mascotwrap">
+		<div id="logo">
+			<img src="/images/landing/BemoLogo_s.png") >
+		</div>
 	   <!-- Begin MailChimp Signup Form -->
 	    <div id="mc_embed_signup" class="well">
 		<form action="http://bemo.us5.list-manage.com/subscribe/post?u=4e832cb6fdc9fa58ac088e75c&amp;id=f1e8c2dab6" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="form validate" target="_blank" novalidate>
@@ -61,5 +87,10 @@
 	    </div>
 	    
 	    <!--End mc_embed_signup-->
-        </div>
+		<div id="mascot">
+			<img src="/images/landing/Bemoscot_s.png") >
+		</div>
+    </div><!-- mascotwrap -->
     </body>
+</html>
+
